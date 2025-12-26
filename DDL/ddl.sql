b5b5bc8dd10926ee25e4ae50f1840feb6279ebb4
Ansh Lamba <anshlambagit@gmail.com>
2025-12-26 11:21:34 -0400

    Update DDL for HOSTS and BOOKINGS tables
    Added comments and modified the BOOKINGS table structure.

--- DDL/ddl.sql
+++ DDL/ddl.sql
@@ -1,3 +1,4 @@
+-- DDL For My Tables
 CREATE OR REPLACE TABLE HOSTS (
     host_id NUMBER,
     host_name STRING,
@@ -23,18 +24,14 @@ CREATE OR REPLACE TABLE LISTINGS (
 );
 
 CREATE OR REPLACE TABLE BOOKINGS (
-    booking_sk NUMBER AUTOINCREMENT,
     booking_id STRING,
-    listing_sk NUMBER,
-    host_sk NUMBER,
-    date_sk NUMBER,
+    listing_id NUMBER,
+    booking_date TIMESTAMP,
     nights_booked NUMBER,
     booking_amount NUMBER,
     cleaning_fee NUMBER,
     service_fee NUMBER,
-    total_amount NUMBER,
     booking_status STRING,
     created_at TIMESTAMP,
     PRIMARY KEY (booking_id)
 );
-
